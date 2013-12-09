453d111c107012ac9b734ff933481ca4363bf6c7
Pothi Kalimuthu <pothi@tinywp.com>
2013-12-09 16:49:33 +0530

    Hide two more settings menu from ewww image optimizer

--- hide-menu.php
+++ hide-menu.php
@@ -11,8 +11,10 @@
  */
 
 function tiny_remove_menus() {
-    // Hide Ewww image optimizer settings that are found in two places
+    // Hide Ewww image optimizer settings that are found in four places
     $page = remove_submenu_page( 'options-general.php', 'ewww-image-optimizer/ewww-image-optimizer.php' );
     $page = remove_submenu_page( 'tools.php', 'ewww-image-optimizer-aux-images' );
+    $page = remove_submenu_page( 'themes.php', 'ewww-image-optimizer-theme-images' );
+    $page = remove_submenu_page( 'upload.php', 'ewww-image-optimizer-bulk' );
 }
 add_action( 'admin_menu', 'tiny_remove_menus', 999 );
