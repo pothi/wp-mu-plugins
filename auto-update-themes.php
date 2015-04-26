179f14a4c94e73f3babf67483222fecd543d4778
Pothi Kalimuthu <pothi@tinywp.com>
2015-04-26 04:56:50 +0000

    Automatic theme updates

---
+++ auto-update-themes.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+ * Plugin Name: Auto theme updates
+ * Plugin URI:  https://github.com/pothi/wordpress-mu-plugins
+ * Version:     0.1
+ * Description: Automatically update all themes
+ * Author:      Pothi Kalimuthu
+ * Author URI:  http://pothi.info
+ * License:     GPL
+ */
+
+
+add_filter( 'auto_update_theme', '__return_false' );
