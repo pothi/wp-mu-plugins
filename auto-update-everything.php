8b517f330c3e8c3d4dec218e0183a63695ba0231
Pothi Kalimuthu <pothi@tinywp.com>
2017-01-08 20:07:14 +0530

    Single plugin to update everything

---
+++ auto-update-everything.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * Plugin Name: Auto update everything in a WordPress site
+ * Plugin URI:  https://github.com/pothi/wordpress-mu-plugins
+ * Version:     0.1
+ * Description: Automatically update WP core (including major versions), themes, and plugins
+ * Author:      Pothi Kalimuthu
+ * Author URI:  http://pothi.info
+ * License:     GPL
+ */
+
+add_filter( 'auto_update_theme', '__return_false' );
+add_filter( 'auto_update_plugin', '__return_true' );
+add_filter( 'allow_major_auto_core_updates', '__return_true' );
